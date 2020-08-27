d82ba9fdc66cf34ce19cf852c528ec6c4c6adcc7
Stanley Hayford <hayfordstanley@gmail.com>
2020-08-27 21:41:42 +0000

    shape interface added to exercise 5 of ninja level 6

--- ninja-level-6/exercise-5.go
+++ ninja-level-6/exercise-5.go
@@ -17,13 +17,23 @@ func (s square) area() float64 {
 	return s.Side * s.Side
 }
 
+type shape interface {
+	area() float64
+}
+
 func (c circle) area() float64 {
 	return math.Pi * c.Radius * c.Radius
+}
+
+func info(s shape)  {
+	fmt.Println(s.area())
 }
 
 func main() {
 	unitCircle := circle{Radius: 1}
 	unitSquare := square{Side: 1}
+	info(unitCircle)
+	info(unitSquare)
 	fmt.Printf("Area of square with radius %v is %v\n", unitSquare.Side, unitSquare.area())
 	fmt.Printf("Area of circle with radius %v is %v", unitCircle.Radius, unitCircle.area())
 }
